66d4332f78e460f4fec9728dc168db04ab82dd8d
Mbu Nyellie <nyelliembu@gmail.com>
2025-04-05 09:58:54 +0100

    New

--- src/components/TutorsDashboard/TutorDashboardLayout.jsx
+++ src/components/TutorsDashboard/TutorDashboardLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import TutorSidebar from "./TutorSidebar";
-import Navbar from "./Navbar";
+import Navbar from "../../components/GigsDashboard/Navbar";
 
 const TutorDashboardLayout = () => {
   return (
